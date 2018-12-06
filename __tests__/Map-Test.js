485362029cbba8475bf666684415816447ab2743
GiacomoMasi <jack953c@gmail.com>
2018-12-06 14:44:05 +0100

    Basic test working
    Fixed environment for tests +basic map test added

---
+++ __tests__/Map-Test.js
@@ -0,0 +1,9 @@
+// __tests__/Intro.test.js
+import React from 'react'
+import ListScreen from '../App/Components/ListScreen'
+import MapView from '../App/Components/MapScreen'
+import renderer from 'react-test-renderer'
+
+test('test', () => {
+
+})
